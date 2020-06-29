934ae2fef567690cc9cc52700da11899a5eeb287
Amit Vashist <amitvashist7@outlook.com>
2020-06-29 12:08:50 +0000

    Updated

--- 03-Basic-commands-execution/first_deployment.tf
+++ 03-Basic-commands-execution/first_deployment.tf
@@ -7,3 +7,7 @@ resource "aws_instance" "example" {
   ami           = "ami-008c6427c8facbe08"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "example" {
+  instance = "${aws_instance.example.id}"
+}
