11e03c3c98792a8dae5c701469419e29b935039d
Amit Vashist <amitvashist7@outlook.com>
2020-06-29 13:18:48 +0000

    04-Resources

--- 04-Resources/resources.tf
+++ 04-Resources/resources.tf
@@ -4,6 +4,15 @@ provider "aws" {
   region     = "us-west-2"
 }
 
+
+resource "aws_instance" "base" {
+  ami           = "ami-0a243dbef00e96192"
+  instance_type = "t2.micro"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
 resource "aws_instance" "frontend" {
   depends_on    = ["aws_instance.backend"]
   ami           = "ami-008c6427c8facbe08"
@@ -22,3 +31,6 @@ resource "aws_instance" "backend" {
     delete = "2h"
   }
 }
+
+
+
